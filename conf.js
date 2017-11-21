1709c7729e77ec16a0e219fe85358a39c137bd3d
Harry Sadler <harrysadlermusic@gmail.com>
2017-11-21 00:32:51 -0800

    add idle creep flag, add code comments

--- conf.js
+++ conf.js
@@ -3,11 +3,14 @@ var conf = {
 
     // creep
     MAX_HARVESTERS: 4,
-    MAX_UPGRADERS: 10,
-    MAX_BUILDERS: 0,
+    MAX_UPGRADERS: 6,
+    MAX_BUILDERS: 2,
 
     // construction sites
-    MAX_EXTENTION_CONSTRUCTION_SITES: 2
+    MAX_EXTENTION_CONSTRUCTION_SITES: 2,
+
+    // flags
+    IDLE_CREEP_FLAG: 'idle'
 
 };
 
--- role.harvester.js
+++ role.harvester.js
@@ -1,6 +1,10 @@
+var conf = require('conf');
+
+
 var roleHarvester = {
 
     run: function(creep) {
+        // harvest from node
         if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
@@ -10,7 +14,9 @@ var roleHarvester = {
                 );
             }
         }
+        // transport to base
         else {
+            // get targets with available capacity
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (
@@ -19,6 +25,7 @@ var roleHarvester = {
                     ) && structure.energy < structure.energyCapacity;
                 }
             });
+            // target with capacity exists
             if(targets.length > 0) {
                 if(
                     creep.transfer(targets[0], RESOURCE_ENERGY) ==
@@ -29,6 +36,13 @@ var roleHarvester = {
                         {visualizePathStyle: {stroke: '#ffffff'}}
                     );
                 }
+            }
+            // move to idle flag
+            else {
+                creep.moveTo(
+                    Game.flags[conf.IDLE_CREEP_FLAG],
+                    {visualizePathStyle: {stroke: '#ffffff'}}
+                );
             }
         }
     }
