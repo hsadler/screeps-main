12dc4dafd7d4e584b9a70495ee76622e652965fe
Harry Sadler <harrypatricksadler@gmail.com>
2017-12-04 17:54:55 -0800

    add implementation pseudo code to road controller

--- ctrl.road.js
+++ ctrl.road.js
@@ -1,5 +1,8 @@
 
 var modelRoom = require('model.room');
+var modelSpawn = require('model.spawn');
+var modelEnergySources = require('model.energy_sources');
+var modelExtension = require('model.extension');
 var modelStorage = require('model.storage');
 
 
@@ -9,21 +12,48 @@ var ctrlRoad = {
 
 
     proc: function() {
-        var path = modelRoom.room.findPath(
-            modelStorage.storage.pos,
-            modelRoom.room.controller.pos
-        );
-        console.log(JSON.stringify(path));
+
+        // TESTING:
+        // var path = modelRoom.room.findPath(
+        //     modelStorage.storage.pos,
+        //     modelRoom.room.controller.pos
+        // );
+        // console.log(JSON.stringify(path));
+
+        this.constructRoomRoads();
     },
 
 
-    contructRoomRoads: function() {
+    constructRoomRoads: function() {
+
+        // roads to construct:
+            // 1. spawn -> energy sources
+            // 2. energy sources -> storage
+            // 3. storage -> controller
+            // 4. storage -> exits
+            // 5. up, down, left, right from each extension
+            // 6. up, down, left, right from each tower
+
+        var paths = [];
 
     },
 
 
-    constructRoadFromPositions: function(posA, posB) {
+    getNextAvailableRoadConstructionPositionFromPaths: function(paths) {
+        // for path in paths:
+        //     for pos in path:
+        //         if not pos.hasStructureOrConstructionSite():
+        //             return pos
+        // return null
+    },
 
+
+    getNextAvailableRoadConstructionPositionFromExtensions: function() {
+        // for extension in room.extensions:
+        //     for pos in extension.getSurroundingPositions():
+        //         if not pos.hasStructureOrConstructionSite:
+        //             return pos
+        // return null
     }
 
 
